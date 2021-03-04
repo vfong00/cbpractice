71785d15886c656235a8fcd51702b00ff081160f
Vincent Fong <vcf5@cornell.edu>
2021-03-04 06:10:25 -0500

    Math practice: - adds/subtracts two letters randomly and checks with user - basic stats

--- cbpractice.py
+++ cbpractice.py
@@ -6,15 +6,16 @@ def main():
     print(chr(27) + "[2J")
     print("Welcome to cbpractice.")
     while True:
-        inp = input("Please choose a mode of practice. \nType 'list' for a list of implemented modes. \nType 'end' to end: ").strip().lower()
+        inp = input("Please choose a mode of practice. \nType 'list' for a list of implemented modes. \nType 'exit' to end session.: ").strip().lower()
         if inp == "list": print("\nCurrent modes: " + ", ".join(modes) + "\n")
-        elif inp == "end": break
+        elif inp == "exit": break
         elif inp == "patterns":
             end = patterns.patterns()
             if end: break
             print(chr(27) + "[2J")
         elif inp == "letnums":
-            letnums.letnums()
+            end = letnums.letnums()
+            if end: break
             print(chr(27) + "[2J")
         else: print("\nSorry, this is not a recognized mode. Try again. \n")
 
--- letnums.py
+++ letnums.py
@@ -6,7 +6,36 @@ alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 sep = "======================================"
 
 def math(streak, correct, total, end, endAll):
-    return
+    letter1 = random.choice(alpha)
+    letter2 = random.choice(alpha)
+    addSub = random.randint(0, 1)
+    operators = [" + ", " - "]
+    start = time.time()
+    inp = input("\'" + letter1 + "\'" + operators[addSub] + "\'" + letter2 + "\'?\n")
+    elapsed = time.time() - start
+    if (inp.lower() == "return" or inp.lower() == "exit"):
+        print("\n" + sep)
+        return streak, correct, total, 0, True, inp.lower() == "exit"
+    if addSub == 0:
+        sum = alpha[(alpha.index(letter1) + alpha.index(letter2)) % 26]
+    else:
+        sum = alpha[(alpha.index(letter1) - alpha.index(letter2)) % 26]
+    if inp.upper() == sum:
+        print("Correct!")
+        correct += 1
+        streak += 1
+        if (streak > 4):
+            print(streak + " streak.")
+    else:
+        print("Incorrect. Correct answer was " + sum)
+        streak = 0
+    total += 1
+    k = input("\nPress ENTER to continue. Type 'return' to return to the menu, and 'exit' to stop the program. \n> ")
+    if (k.strip().lower() == "return" or k.strip().lower() == "exit"):
+        print("\n" + sep)
+        return streak, correct, total, elapsed, True, k.strip().lower() == "exit"
+    print(sep)
+    return streak, correct, total, elapsed, end, endAll
 
 def id(streak, correct, total, end, endAll):
     return
@@ -23,15 +52,34 @@ def letnums():
     print("Welcome to letter-numbers practice.")
     print("There are two modes: 'math' for addition/subtraction practice, and 'id' to quick id")
     print("Enter 'return' at any time to return to the menu, or 'exit' to stop this program.")
-    mode = input("Select mode: ")
-    while mode != "math" or mode != "id":
+    mode = input("Select mode: ").strip().lower()
+    while mode != "math" and mode != "id" and mode != "return" and mode != "exit":
         mode = input("Invalid argument. Select mode: ")
     print(sep)
 
     while not end:
-        if mode == "math":
+        if mode == "return" or mode == "exit":
+            return mode == "exit"
+        elif mode == "math":
             streak, correct, total, time, end, endAll = math(streak, correct, total, end, endAll)
         elif mode == "id":
             streak, correct, total, time, end, endAll = id(streak, correct, total, end, endAll)
         maxStreak = max(streak, maxStreak)
         totalTime += time
+
+    # print ending stats
+    print("SESSION SUMMARY: ")
+    print("Correct: " + str(correct))
+    print("Total: " + str(total))
+    if total != 0:
+        print("Percentage: " + str(round(correct / total * 100, 2)) + "%")
+        print("Average time: " + str(round(totalTime / total, 2)) + "s")
+    else:
+        print("Percentage: 0%")
+        print("Average time: 0s")
+    print("Highest streak: " + str(maxStreak))
+    print("Goodbye :)")
+    print(sep + "\n")
+    if not endAll:
+        input("(Press ENTER to return)")
+    return endAll
